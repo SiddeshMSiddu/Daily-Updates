1ebe830cf62379afe95d34ffee74e362ca317574
Siddesh4877 <siddeshm4877@gmail.com>
2024-12-07 14:26:04 +0530

    Switch Program

--- day07dec/src/App.js
+++ day07dec/src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import ProfileCreation from './ProfileCreation'
+import SwitchProgram from './SwitchProgram'
+
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
       <ProfileCreation/>
+      <SwitchProgram/>
     </div>
   );
 }
