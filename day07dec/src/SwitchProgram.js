40488e0a294d3de112690e61d45bdbde28bf5d53
Siddesh4877 <siddeshm4877@gmail.com>
2024-12-07 14:26:13 +0530

    Switch

---
+++ day07dec/src/SwitchProgram.js
@@ -0,0 +1,29 @@
+function Grade({marks}){
+    const Msg=(marks)=>{
+    switch(true){
+        case (marks>=35 && marks<=55): return "Just pass";
+        case (marks>55 && marks<=65): return "first class"; 
+        case (marks>65 && marks<=84): return "Second Class";
+        case (marks>=85 && marks<=100): return "Distinction";
+        case (marks<35):return "Better Luck Next Time"
+        default: return "Invali marks"
+    
+    }
+};
+    
+    return(
+        <>
+        <h1>marks:</h1>
+        <p>{Msg(marks)}</p>
+        </>
+    );
+        
+}
+
+export  default function Message(){
+    return(
+        <>
+        <Grade marks="66"/>
+        </>
+    )
+}
